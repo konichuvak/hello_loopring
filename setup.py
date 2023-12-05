dd593334c4b55f9341a7f452871a232c8171d05e
konichuvak <konichuvak@proton.me>
2023-12-05 15:44:38 -0500

    [Packaging] Adds repo version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='hello_loopring',
-    version='',
+    version='0.0.1',
     description='Loopring V3 api sample with sdk',
     author='Loopring',
     python_requires='>=3.7',
